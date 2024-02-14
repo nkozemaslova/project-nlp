8bc9f8b8bea78d31a355069d7a8ceb6cfb86c77e
natalya.kozemaslova <knw333@mail.ru>
2024-02-14 22:25:51 +0300

    6

--- infer.py
+++ infer.py
@@ -1,29 +1,35 @@
+import hydra
 import pandas as pd
 from catboost import CatBoostClassifier, Pool
+from omegaconf import DictConfig, OmegaConf
 from preprocessing import preprocess_data_test, preprocess_test
 
-model = CatBoostClassifier()
-model.load_model("model.bin")
 
-test = pd.read_csv("./data/test_df.csv", index_col=0)
+@hydra.main(version_base=None, config_path="conf", config_name="config")
+def main(cfg: DictConfig) -> None:
+    OmegaConf.to_yaml(cfg)
+
+    model = CatBoostClassifier()
+    model.load_model("model3.bin")
+
+    test = pd.read_csv("./data/test_df.csv", index_col=0)
+
+    test = preprocess_data_test(test)
+    test = preprocess_test(test)
+
+    test.drop("date", axis=1, inplace=True)
+
+    test_pool = Pool(
+        test,
+        cat_features=cfg.features.cat,
+        text_features=cfg.features.text,
+    )
+    pred = model.predict(test_pool)
+
+    df_pred = pd.DataFrame(pred, columns=["predictions"])
+
+    return df_pred.to_csv("predictions.csv", index=False)
 
 
 if __name__ == "__main__":
-    test = preprocess_data_test(test)
-
-test = preprocess_test(test)
-
-test.drop("date", axis=1, inplace=True)
-
-test_pool = Pool(
-    test,
-    cat_features=["bank", "time_day", "year", "month", "day"],
-    text_features=["feeds", "lemmas"],
-)
-pred = model.predict(test_pool)
-
-# Create a DataFrame from the predictions
-df_pred = pd.DataFrame(pred, columns=["predictions"])
-
-# Save the DataFrame to a .csv file
-df_pred.to_csv("predictions.csv", index=False)
+    main()
--- train.py
+++ train.py
@@ -1,74 +1,66 @@
+import hydra
 import pandas as pd
 from catboost import CatBoostClassifier, Pool
+from omegaconf import DictConfig, OmegaConf
 from preprocessing import first_preprocess, preprocess_data
 from sklearn.model_selection import train_test_split as tts
 
-df = pd.read_csv("./data/train_df.csv")
 
-df = first_preprocess(df)
+@hydra.main(version_base=None, config_path="conf", config_name="config")
+def main(cfg: DictConfig) -> None:
+    OmegaConf.to_yaml(cfg)
+
+    def fit_model(train_pool, validation_pool, **kwargs):
+        model = CatBoostClassifier(
+            iterations=cfg.params.iterations,
+            learning_rate=cfg.params.learning_rate,
+            eval_metric=cfg.params.eval_metric,
+            use_best_model=cfg.params.use_best_model,
+            task_type=cfg.params.task_type,
+            **kwargs
+        )
+
+        return model.fit(
+            train_pool,
+            eval_set=validation_pool,
+            verbose=cfg.params.verbose,
+        )
+
+    df = pd.read_csv("./data/train_df.csv")
+    df = first_preprocess(df)
+    df = preprocess_data(df)
+
+    df.reset_index(drop=True, inplace=True)
+
+    df_train_val = df[cfg.dataset.columns]
+    y_train_val = df["grades"]
+    X_train, X_val, y_train, y_val = tts(
+        df_train_val,
+        y_train_val,
+        shuffle=True,
+        stratify=y_train_val,
+        train_size=0.999,
+    )
+
+    train_pool = Pool(
+        X_train,
+        y_train,
+        cat_features=cfg.features.cat,
+        text_features=cfg.features.text,
+    )
+
+    validation_pool = Pool(
+        X_val,
+        y_val,
+        cat_features=cfg.features.cat,
+        text_features=cfg.features.text,
+    )
+
+    print("Train dataset shape: {}\n".format(train_pool.shape))
+
+    model = fit_model(train_pool, validation_pool)
+    return model.save_model("model.bin")
 
 
 if __name__ == "__main__":
-    df = preprocess_data(df)
-
-
-def fit_model(train_pool, validation_pool, **kwargs):
-    model = CatBoostClassifier(
-        iterations=100,
-        learning_rate=0.009,
-        eval_metric="MultiClass",
-        use_best_model=True,
-        task_type="CPU",
-        **kwargs
-    )
-
-    return model.fit(
-        train_pool,
-        eval_set=validation_pool,
-        verbose=100,
-    )
-
-
-df.reset_index(drop=True, inplace=True)
-
-df_train_val = df[
-    [
-        "bank",
-        "feeds",
-        "lemmas",
-        "year",
-        "month",
-        "day",
-        "time_day",
-        "sym_len",
-        "word_len",
-    ]
-]
-y_train_val = df["grades"]
-X_train, X_val, y_train, y_val = tts(
-    df_train_val,
-    y_train_val,
-    shuffle=True,
-    stratify=y_train_val,
-    train_size=0.999,
-)
-
-
-train_pool = Pool(
-    X_train,
-    y_train,
-    cat_features=["bank", "time_day", "year", "month", "day"],
-    text_features=["lemmas", "feeds"],
-)
-
-validation_pool = Pool(
-    X_val,
-    y_val,
-    cat_features=["bank", "time_day", "year", "month", "day"],
-    text_features=["lemmas", "feeds"],
-)
-
-print("Train dataset shape: {}\n".format(train_pool.shape))
-
-model = fit_model(train_pool, validation_pool)
-model.save_model("model.bin")
+    main()
